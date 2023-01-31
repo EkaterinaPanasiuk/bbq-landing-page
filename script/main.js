67332a4dea3c84be218e9a2f9f2c372f8aad1317
Ekaterina Panasiuk <ekaterinapanasiuk29@gmail.com>
2023-01-31 13:34:28 +0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+
+let body=document.querySelector('body');
 let burger= document.querySelector('.header__burger');
 burger.addEventListener('click', function(){
     let menu=document.querySelector('.header__menu');
@@ -5,3 +7,44 @@ burger.addEventListener('click', function(){
     document.querySelector('body').classList.toggle('stopscroll');
     burger.classList.toggle('cross');
 })
+
+//----------------------preloader-----------------------------
+
+
+function load(){
+    let preloader=document.querySelector('.preloader');
+    preloader.classList.add('hide');
+    setTimeout(function(){
+        preloader.classList.add('none')
+    },1000)
+}
+
+window.onload=load();
+
+//------------------------zoom----------------------
+window.addEventListener('mouseover', function(event){
+    if(event.target.classList.contains('about__img')){
+        let img=event.target;
+        let card=img.closest('.about__item');
+        card.classList.add('zoom');
+        body.classList.add('stopscroll')
+        document.addEventListener('keydown', function(event){
+            console.log(event.code)
+            if(event.code=="Escape"){
+                card.classList.remove('zoom');
+                body.classList.remove('stopscroll')
+            }
+        })
+        }
+ })
+
+ window.addEventListener('mouseout', function(event){
+    if(event.target.classList.contains('about__img')){
+        let img=event.target;
+        let card=img.closest('.about__item');
+        card.classList.remove('zoom');
+        body.classList.remove('stopscroll')
+    }
+ })
+ 
+
